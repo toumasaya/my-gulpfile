2e5907804ed8886f6471aca0ea7e7763e9f03c79
toumasaya <luffy.w.pirate@gmail.com>
2016-12-15 02:13:01 +0800

    Create Sass task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var browserSync = require('browser-sync');
 var del = require('del');
 var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
 var cached = require('gulp-cached');
 var changed = require('gulp-changed');
+var cssnano = require('gulp-cssnano');
 var debug = require('gulp-debug');
 var filter = require('gulp-filter');
 var gulpIf = require('gulp-if');
@@ -10,6 +12,8 @@ var notify = require('gulp-notify');
 var plumber = require('gulp-plumber');
 var pug = require('gulp-pug');
 var pugInheritance = require('gulp-pug-inheritance');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var runSequence = require('run-sequence');
 
 // Development tasks
@@ -67,9 +71,45 @@ gulp.task('setWatch', function() {
   global.isWatching = true;
 });
 
+// sass
+gulp.task('sass', function(){
+  return gulp.src('app/sass/**/*.{scss,sass}')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .on('error', sass.logError)
+    .pipe(sourcemaps.write())
+    .pipe(debug({title: 'debug-sass'}))
+    .pipe(gulp.dest('app/css'))
+    .pipe(notify({message: 'Server "<%= file.path %>"'}))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
 // Cleaning
 gulp.task('clean', function(){
   return del(['dist/**/*']);
+});
+
+// Optimizeing tasks
+// -----------------
+
+// Optimizeing CSS with cssnano
+gulp.task('cssnano', function(){
+  return gulp.src('app/css/**/*.css')
+    .pipe(cssnano({
+      zindex: false // fixed the z-index bug
+    }))
+    .pipe(debug({title: 'debug-css'}))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(notify({message: 'Server "<%= file.path %>"'}))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
 // Watchers
@@ -77,15 +117,17 @@ gulp.task('clean', function(){
 
 gulp.task('watch', ['setWatch', 'pug'], function(){
   gulp.watch('app/pug/**/*.pug', ['pug']);
+  gulp.watch('app/sass/**/*.{scss,sass}', ['sass']);
+  gulp.watch('app/css/**/*.css', ['cssnano']);
 });
 
 // Build Sequence
 // -------------------
 
 gulp.task('default', function(){
-  runSequence('watch', ['pug', 'browserSync']);
+  runSequence('watch', ['pug', 'sass','browserSync']);
 });
 
 gulp.task('build', function(){
-  runSequence('clean', ['pug']);
+  runSequence('clean', ['pug', 'sass', 'cssnano']);
 });
